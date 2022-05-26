188b0344b40ebfe4de7a410dc52e4ac95b6a8741
Maddy <madisonschaaf48@gmail.com>
2022-05-25 20:22:58 -0600

    set up indexDB

---
+++ public/js/idb.js
@@ -0,0 +1,72 @@
+
+//variable holding db connection
+let db  
+
+const request = indexedDB.open('budget_tracker', 1)
+
+
+request.onupgradeneeded = function(event) {
+    const db = event.target.result
+    db.createObjectStore('new_item', {autoIncrement: true})
+}
+
+request.onsuccess = function(event) {
+    db= event.target.result
+
+
+if(navigator.online) {
+    uploadItem()
+}
+}
+
+request.onerror = function(event) {
+    console.log(event.target.errorCode)
+}
+
+
+function saveRecord(record) {
+   
+    const transaction = db.transaction(['new_item'], 'readwrite')
+
+    const itemObjectStore = transaction.objectStore('new_item')
+
+    itemObjectStore.add(record)
+}
+
+//this function is not working!!!
+
+function uploadItem() {
+    const transaction = db.transaction(['new_item'], 'readwrite')
+    const itemObjectStore = transaction.objectStore('new_item')
+
+    const getAll = itemObjectStore.getAll()
+
+    getAll.onsuccess = function() {
+        if(getAll.result.length > 0) {
+            //check url
+            fetch('/api/transaction', {
+                method: 'POST',
+                body: JSON.stringify(getAll.result),
+                headers: {
+                    Accept: 'application/json, text/plain, */*',
+                    'Content-Type': 'application/json'
+                }
+            })
+            .then(response => response.json())
+            .then(serverResponse => {
+                if(serverResponse.message) {
+                    throw new Error(serverResponse)
+                }
+
+                const transaction = db.transaction(['new_item'], 'readwrite')
+                const itemObjectStore = transaction.objectStore('new_item')
+                itemObjectStore.clear()
+            })
+            .catch(err => {
+                console.log(err)
+            })
+        }
+    }
+}
+
+window.addEventListener('online', uploadItem)
